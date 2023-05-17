7fab665e08e89acf3786976a19badeff6e8bbc16
Adamkelany <122812133+Adamkelany@users.noreply.github.com>
2023-05-17 22:48:11 +0300

    Add files via upload

---
+++ InfixToPostfix/src/main/java/com/mycompany/infixtopostfix/InfixToPostfix.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.infixtopostfix;
+
+/**
+ *
+ * @author AGHA SMART STORE
+ */
+
+import java.util.Scanner;
+
+public class InfixToPostfix {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an infix expression: ");
+        String infixExpression = scanner.nextLine();
+        
+        String postfixExpression = convertToPostfix(infixExpression);
+        System.out.println("Postfix expression: " + postfixExpression);
+        
+        int result = evaluatePostfix(postfixExpression);
+        System.out.println("Result: " + result);
+    }
+    
+    public static String convertToPostfix(String infixExpression) {
+        NewStack stack = new NewStack(infixExpression.length());
+        StringBuilder postfixExpression = new StringBuilder();
+        
+        for (int i = 0; i < infixExpression.length(); i++) {
+            char c = infixExpression.charAt(i);
+            
+            if (Character.isDigit(c)) {
+                postfixExpression.append(c);
+            } else if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    postfixExpression.append(stack.pop());
+                }
+                if (!stack.isEmpty() && stack.peek() != '(') {
+                    throw new IllegalArgumentException("Invalid expression");
+                } else {
+                    stack.pop();
+                }
+            } else {
+                while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek())) {
+                    postfixExpression.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+        
+        while (!stack.isEmpty()) {
+            postfixExpression.append(stack.pop());
+        }
+        
+        return postfixExpression.toString();
+    }
+    
+    public static int evaluatePostfix(String postfixExpression) {
+        NewStack stack = new NewStack(postfixExpression.length());
+        
+        for (int i = 0; i < postfixExpression.length(); i++) {
+            char c = postfixExpression.charAt(i);
+            
+            if (Character.isDigit(c)) {
+                stack.push((char) (c - '0'));
+            } else {
+                int operand2 = stack.pop();
+                int operand1 = stack.pop();
+                
+                switch (c) {
+                    case '+':
+                        stack.push((char) (operand1 + operand2));
+                        break;
+                    case '-':
+                        stack.push((char) (operand1 - operand2));
+                        break;
+                    case '*':
+                        stack.push((char) (operand1 * operand2));
+                        break;
+                    case '/':
+                        stack.push((char) (operand1 / operand2));
+                        break;
+                }
+            }
+        }
+        
+        return stack.pop();
+    }
+    
+    public static int precedence(char c) {
+        switch (c) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+            default:
+                return -1;
+        }
+    }
+}
---
+++ InfixToPostfix/src/main/java/com/mycompany/infixtopostfix/NewStack.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.infixtopostfix;
+
+/**
+ *
+ * @author AGHA SMART STORE
+ */
+public class NewStack {
+    private char[] arr;
+    private int top;
+    private int size;
+
+    public NewStack(int size) {
+        this.arr = new char[size];
+        this.top = -1;
+        this.size = size;
+    }
+
+    public void push(char value) {
+        if (isFull()) {
+            System.out.println("Stack is full. Cannot push element");
+            return;
+        }
+        arr[++top] = value;
+    }
+
+    public char pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot pop element");
+            return '\0';
+        }
+        return arr[top--];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == size - 1;
+    }
+
+    public char peek() {
+        if (isEmpty()) {
+            System.out.println("Stack Underflow!");
+            return '\0';
+        }
+        return arr[top];
+    }
+}
